30383c70a0da49514722ad6ce10723379df000bf
Hammad-23 <hsyed2633@gmail.com>
2022-01-19 22:28:43 +0500

    project setup of food hub

---
+++ src/config/router.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "../screen/home/home";
+
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/shop">
+           
+          </Route>
+          <Route exact path="/products">
+          
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
---
+++ src/screen/home/home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./home.css";
+import { Col, Row, Container } from "react-bootstrap";
+
+
+export default function Home() {
+  
+
+  return (
+    <>
+      <h1>Home Page</h1>
+      
+      
+    </>
+  );
+}
